3f62be606058c1f51f741b3f4d761318ea3b2a75
sunpaaaa <sunpaaaa@gmail.com>
2024-03-06 09:20:42 +0900

    data fetching in client component

--- app/(Home)/page.tsx
+++ app/(Home)/page.tsx
@@ -1,13 +1,25 @@
-import { Metadata } from 'next';
+'use client';
 
-export const metadata: Metadata = {
-  title: 'Home',
-};
+import { useEffect, useState } from 'react';
 
 export default function Hello() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const res = await fetch(
+      'https://nomad-movies.nomadcoders.workers.dev/movies'
+    );
+    const json = await res.json();
+    setMovies(json);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
   return (
     <div>
       <h1>Home🏡</h1>
+      <div>{isLoading ? 'loading...' : JSON.stringify(movies)}</div>
     </div>
   );
 }
